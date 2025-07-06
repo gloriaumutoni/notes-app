d95573086f4dafac63c64dcd6600f4957e1cea04
gloriaumutoni <gloria.umutoni54@gmail.com>
2025-07-07 00:09:05 +0200

    chore: change package name

--- lib/widgets/auth_wrapper.dart
+++ lib/widgets/auth_wrapper.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
 import '../screens/sign_in_screen.dart';
-import '../screens/notes_screen.dart';
+import '../screens/notes.dart';
 
 class AuthWrapper extends StatelessWidget {
   const AuthWrapper({super.key});
